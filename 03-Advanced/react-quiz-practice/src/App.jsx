791e4900793808c7387134f0c99d1b76a7bbbf02
saurav-verma-01 <sauravkumarverma22@gmail.com>
2024-06-21 21:07:05 +0530

    useReducer practice using dateCounter Component

---
+++ 03-Advanced/react-quiz-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import DareCounter from "./DateCounter";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <DareCounter />
+    </div>
+  );
+};
+
+export default App;
