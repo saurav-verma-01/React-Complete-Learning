5be0df1287b85ff3e62d8c5c469dc543016ece56
saurav-verma-01 <sauravkumarverma22@gmail.com>
2024-06-21 18:30:30 +0530

    repeating useReducer Hook

--- 03-Advanced/react-quiz/src/App.jsx
+++ 03-Advanced/react-quiz/src/App.jsx
@@ -1,8 +1,22 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import MainComp from "./MainComp";
+
 const App = () => {
+  useEffect(() => {
+    fetch("http://localhost:3000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error", err));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <MainComp>
+        <p>1/15</p>
+        <p>Question?</p>
+      </MainComp>
     </div>
   );
 };
---
+++ 03-Advanced/react-quiz/src/MainComp.jsx
@@ -0,0 +1,5 @@
+const MainComp = ({ children }) => {
+  return <main className="main">{children}</main>;
+};
+
+export default MainComp;
