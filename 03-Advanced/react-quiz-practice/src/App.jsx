464457424e2655e47f8fa3a9625e49931fa1d94f
saurav-verma-01 <sauravkumarverma22@gmail.com>
2024-06-23 14:29:38 +0530

    loaded data with json server and working with reduced to manage app state

--- 03-Advanced/react-quiz-practice/src/App.jsx
+++ 03-Advanced/react-quiz-practice/src/App.jsx
@@ -1,10 +1,46 @@
-import DareCounter from "./DateCounter";
-import React from "react";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import MainContainer from "./MainContainer";
+
+const initialState = {
+  questions: [],
+  // Statuses --> Loading..., Error..., Ready..., Active..., Finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DareCounter />
+    <div className="app">
+      <Header />
+      <MainContainer>
+        <p>1/15</p>
+        <p>Question?</p>
+      </MainContainer>
     </div>
   );
 };
---
+++ 03-Advanced/react-quiz-practice/src/MainContainer.jsx
@@ -0,0 +1,5 @@
+const MainContainer = ({ children }) => {
+  return <main className="main">{children}</main>;
+};
+
+export default MainContainer;
